d558fce291cbe144b1cea7e1f2c40d0bb0c220da
Andrew Yalung <yalung@usc.edu>
2019-12-25 09:03:13 -0800

    added some styling

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,26 +6,46 @@ import { View, Group, Trophy, Menu } from 'grommet-icons';
 const columns= [
   {
     property: 'date',
-    header: 'Date',
-    primary: true
+    header: <Text size="small" weight="bold" style={{ letterSpacing: '3px'}}>DATE</Text>,
+    render: (datum) => (
+      <Text size="small" weight="bold">{datum.date}</Text>
+    )
   }, {
     property: 'category',
-    header: 'Category',
+    header: <Text size="small" weight="bold" style={{ letterSpacing: '3px' }} color="dark-3">CATEGORY</Text>,
+    render: (datum) => (
+      <Text size="small">{datum.category}</Text>
+    )
   }, {
     property: 'title',
-    header: 'Title',
+    header: <Text size="small" weight="bold" style={{ letterSpacing: '3px' }} color="dark-3">TITLE</Text>,
+    render: (datum) => (
+      <Text size="small">{datum.title}</Text>
+    )
   }, {
     property: 'duration',
-    header: 'Duration',
+    header: <Text size="small" weight="bold" style={{ letterSpacing: '3px' }} color="dark-3">DURATION</Text>,
+    render: (datum) => (
+      <Text size="small">{datum.duration}</Text>
+    )
   }, {
     property: 'viewcount',
-    header: 'Average Viewers',
+    header: <Text size="small" weight="bold" style={{ letterSpacing: '3px' }} color="dark-3">AVG VIEWERS</Text>,
+    render: (datum) => (
+      <Text size="small">{datum.viewcount}</Text>
+    )
   }, {
     property: 'followers',
-    header: 'Follower Gained',
+    header: <Text size="small" weight="bold" style={{ letterSpacing: '3px' }} color="dark-3">FOLLOWERS</Text>,
+    render: (datum) => (
+      <Text size="small" color="status-ok">{datum.followers}</Text>
+    )
   }, {
     property: 'subscribers',
-    header: 'Subscribers Gained',
+    header: <Text size="small" weight="bold" style={{ letterSpacing: '3px' }} color="dark-3">SUBSCRIBERS</Text>,
+    render: (datum) => (
+      <Text size="small" color="status-critical">{datum.subscribers}</Text>
+    )
   }
 ];
 
@@ -37,7 +57,16 @@ const data = [
     duration: '8 hrs',
     viewcount: '11.1k',
     followers: '+1100',
-    subscribers: '320'
+    subscribers: '-320'
+  },
+  {
+    date: '11/2/2019',
+    category: 'Fortnite',
+    title: 'Duo Arenas',
+    duration: '8 hrs',
+    viewcount: '11.1k',
+    followers: '+1100',
+    subscribers: '-320'
   }
 ]
 
@@ -80,11 +109,11 @@ export default class Home extends Component {
             direction="row"
             align="center"
             justify="between"
-            pad={{ horizontal: "medium", vertical: "small" }}
+            pad={{ horizontal: "medium", vertical: "xsmall" }}
             border={{color: 'light-4', size: 'xsmall', style:'solid', side: 'bottom'}}
           >
             <Button>
-              <Text size="large">Dashboard</Text>
+              <Heading size="small" level={2} margin="xsmall" pad="xsmall">Dashboard</Heading>
             </Button>
             <Text>my@email</Text>
           </Box>
@@ -118,7 +147,7 @@ export default class Home extends Component {
               </Box>
             </Button>
           </Box>
-          <Box gridArea="main" justify="start" align="start" pad="large" background="#F4F7FD">
+          <Box gridArea="main" justify="start" align="start" pad={{ horizontal: "large", vertical: "medium" }} background="#F4F7FD">
             <Heading size="small" level={2} margin="xsmall">Overview</Heading>
             <Box direction="row-responsive" pad="small" gap="xlarge" fill="horizontal">
               <Box 
@@ -171,8 +200,17 @@ export default class Home extends Component {
               </Box>
             </Box>
             <Heading size="small" level={2} margin="small">Recent Streams</Heading>
-            <Box elevation="xsmall" round="xsmall" pad="small" fill="horizontal" background="white"> 
-              <DataTable columns={columns} data={data} />
+            <Box round="xsmall" pad="small" fill="horizontal"> 
+              <DataTable 
+                columns={columns} 
+                data={data} 
+                background={{
+                  header: "#F4F7FD",
+                  body: "white"
+                }}
+                pad={{body: "medium"}}
+                border={{body: "bottom"}}
+              />
             </Box>
           </Box>
         </Grid>
