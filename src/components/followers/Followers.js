7e845128c9802fe49a91e822b4bb6f4d72050fc9
Andrew Yalung <andy.yalung1@gmail.com>
2020-01-01 14:00:28 -0700

    chart

--- src/components/followers/Followers.js
+++ src/components/followers/Followers.js
@@ -2,44 +2,97 @@ import React, { Component } from 'react';
 import Nav from '../nav/Nav';
 
 import { Box, Chart, RangeSelector, Stack, Text } from 'grommet'
+import { Group } from 'grommet-icons';
+import moment from 'moment';
 
 export default class Followers extends Component {
   constructor() {
     super();
     this.state = {
       range: [0, 5],
-      val: []
+      val: [],
+      hover: undefined,
+      thickness: 'xsmall',
+      loaded: false
     }
     this.onChange = this.onChange.bind(this);
+    this.onHover = this.onHover.bind(this);
   }
 
   componentDidMount() {
     const val = [];
-    for(let i = 0; i < 200; i++) {
-      val.push({ value: [i, Math.floor(Math.random() * Math.floor(100))]});
+    for (let i = 0; i < 100; i++) {
+      const today = moment();
+      val.push({ value: [i, Math.floor(Math.random() * Math.floor(100))], label: moment(today).add(i, 'days').format("L") });
     }
-    this.setState({ range: [0, this.state.val.length-1], val});
+    this.setState({ range: [val[0].value[0], val[val.length - 1].value[0]], val, loaded: true });
   }
 
   onChange(range) {
-    this.setState({ range });
+    let thickness = "small"
+    const diff = range[1] - range[0];
+    if (diff > 100) {
+      thickness = "hair";
+    } else if (diff > 75) {
+      thickness = "xxsmall";
+    } else if (diff > 50) {
+      thickness = "xsmall";
+    }
+    this.setState({ range, thickness });
+  }
+
+  onHover = (value) => (over) => {
+    this.setState({ hover: over ? value : undefined });
   }
 
   render() {
-    const { range, val } = this.state;
-    console.log(range, val.length, val.slice(range[0], range[1]+1));
-    return (
-      <Nav title="Followers">
-        <div>Followers Page</div>
-        <Box elevation="small" pad="medium" background="white" round="xsmall" width="full">
-            <Chart
-              type="bar"
-              color="accent-2"
-              values={val.slice(range[0], range[1]+1)}
-              size={{ width: "full", height: "medium" }}
-              bounds={[[range[0], range[1]], [0,100]]}
-              thickness="xsmall"
-            />
+    const { range, val, hover, thickness, loaded } = this.state;
+    if (loaded) {
+      return (
+        <Nav title="Followers">
+          <Box elevation="small" pad="medium" background="white" round="xsmall" width="full">
+            <Box width="full" pad={{ horizontal: "small", vertical: "small" }} background="white" round="small" align="start">
+              <Box direction="row" gap="small" fill>
+                <Box elevation="medium" pad="small" round="full">
+                  <Group size="medium" color="#0C81EB" />
+                </Box>
+                <Box pad={{ vertical: "small" }}>
+                  <Text size="large" weight="bold">{val[val.length - 1].value[1]} Current Followers</Text>
+                </Box>
+              </Box>
+            </Box>
+            <Stack guidingChild="first" interactiveChild="first">
+              <Box size="full">
+                <Chart
+                  type="bar"
+                  values={val.slice(range[0], range[1] + 1).map(v => ({
+                    ...v,
+                    onHover: this.onHover(v)
+                  }))}
+                  size={{ width: "full", height: "medium" }}
+                  bounds={[[range[0], range[1]], [0, 100]]}
+                  thickness={thickness}
+                  color={{ color: "#0C81EB", opacity: "strong" }}
+                  round
+                />
+              </Box>
+              {hover && (
+                <Box elevation="medium" width="small" pad={{ horizontal: "small", vertical: "small" }} background="white" round="small" align="start">
+                  <Box direction="row" gap="xsmall" fill>
+                    <Group size="18px" color="#0C81EB" />
+                    <Text size="small" weight="bold" textAlign="center">{hover.value[1]} Followers</Text>
+                  </Box>
+                  <Box direction="row" gap="xsmall" fill>
+                    <Group size="18px" color={{ color: "#0C81EB", opacity: "weak" }} />
+                    <Text size="xsmall" color="dark-3" textAlign="center">{hover.label}</Text>
+                  </Box>
+                </Box>
+              )}
+            </Stack>
+            <Box height="xxxsmall" width="full" direction="row" justify="between">
+              <Text size="xsmall" weight="bold">{val[range[0]].label}</Text>
+              <Text size="xsmall" weight="bold">{val[range[1]].label}</Text>
+            </Box>
             <Box height="xxsmall" width="full">
               <Stack fill>
                 <Chart
@@ -47,20 +100,24 @@ export default class Followers extends Component {
                   values={val}
                   size={{ width: "full", height: "xxsmall" }}
                   thickness="xxsmall"
+                  round
+                  color="light-3"
                 />
                 <RangeSelector
                   direction="horizontal"
-                  min={0}
-                  max={val.length-1}
+                  min={val[0].value[0]}
+                  max={val[val.length - 1].value[0]}
                   size="full"
                   values={range}
                   onChange={this.onChange}
-                  color="accent-2"
+                  color="#0C81EB"
                 />
-              </Stack>   
+              </Stack>
             </Box>
-        </Box>
-      </Nav>
-    );
+          </Box>
+        </Nav>
+      );
+    }
+    return <div></div>
   }
 }
